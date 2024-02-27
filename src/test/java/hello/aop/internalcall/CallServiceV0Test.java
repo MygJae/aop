b0bff4ebebed6328829bd5f04a6a12e1d2dda5f5
MygJae <50861514+mygjae@users.noreply.github.com>
2024-02-27 16:09:50 +0900

    스프링 AOP 주의사항 - 대안 1,2,3, - 자기 자신 주입, 지연 조회, 구조 변경

---
+++ src/test/java/hello/aop/internalcall/CallServiceV0Test.java
@@ -0,0 +1,16 @@
+package hello.aop.internalcall;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CallServiceV0Test {
+
+    @Test
+    void external() {
+    }
+
+    @Test
+    void internal() {
+    }
+}
